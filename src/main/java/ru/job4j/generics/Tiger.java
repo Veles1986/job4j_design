db8e4ceda09ea3abe39858792330414185dfcd5d
Veles1986 <ilyos.rafa@mail.com>
2024-08-21 22:20:43 +0500

    1. Что такое обобщенные типы (generics) [#4952]

---
+++ src/main/java/ru/job4j/generics/Tiger.java
@@ -0,0 +1,11 @@
+package ru.job4j.generics;
+
+public class Tiger extends Predator {
+    public Tiger() {
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
