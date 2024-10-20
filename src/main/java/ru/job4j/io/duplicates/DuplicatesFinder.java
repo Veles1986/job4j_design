4085874e9e3ab44fccda9861be5041e4042b9e72
Veles1986 <ilyos.rafa@mail.com>
2024-10-20 15:42:26 +0500

    11. Поиск дубликатов [#315066 #517291]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -1,5 +1,4 @@
 package ru.job4j.io.duplicates;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -1,22 +1,34 @@
 package ru.job4j.io.duplicates;
-
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    private Map<FileProperty, ArrayList<Path>> mapOfFiles = new HashMap<>();
 
-    private Set<FileProperty> setOfFiles = new HashSet<>();
+    public static void main(String[] args) throws IOException {
+        DuplicatesVisitor duplicatesVisitor = new DuplicatesVisitor();
+        Files.walkFileTree(Path.of("./"), duplicatesVisitor);
+        duplicatesVisitor.printDuplicates();
+    }
+
+    public void printDuplicates() {
+        for (FileProperty fileProperty : mapOfFiles.keySet()) {
+            if (mapOfFiles.get(fileProperty).size() > 1) {
+                for (Path path : mapOfFiles.get(fileProperty)) {
+                    System.out.println(path);
+                }
+            }
+        }
+    }
 
     @Override
-    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        if (!setOfFiles.add(new FileProperty(file.toFile().length(), file.getFileName().toString()))) {
-            System.out.println(file.toAbsolutePath());
-        }
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        mapOfFiles.computeIfAbsent(new FileProperty(file.toFile().length(), file.getFileName().toString()), initialize -> new ArrayList<>()).add(file.toAbsolutePath());
         return FileVisitResult.CONTINUE;
     }
 }
