4315b71e592c092333e276b4f5d3d4f5a5116a85
Veles1986 <ilyos.rafa@mail.com>
2025-02-16 21:58:32 +0500

    Обход дерева в глубину [#504974]

--- src/main/java/ru/job4j/newcoll/tree/TreeUtils.java
+++ src/main/java/ru/job4j/newcoll/tree/TreeUtils.java
@@ -1,6 +1,7 @@
 package ru.job4j.newcoll.tree;
 
 import ru.job4j.collection.SimpleQueue;
+import ru.job4j.collection.SimpleStack;
 
 import java.util.*;
 
@@ -33,6 +34,65 @@ public class TreeUtils<T> {
             result.add(subNode.getValue());
             for (Node<T> node : subNode.getChildren()) {
                 queue.push(node);
+            }
+        }
+        return result;
+    }
+
+    public boolean add(Node<T> root, T parent, T child) {
+        Optional<Node<T>> parentNode = findByKey(root, parent);
+        Optional<Node<T>> childNode = findByKey(root, child);
+        if (parentNode.isPresent() && childNode.isEmpty()) {
+            parentNode.get().getChildren().add(new Node<>(child));
+            return true;
+        }
+        return false;
+    }
+
+    public Optional<Node<T>> findByKey(Node<T> root, T key) {
+        if (root == null) {
+            throw new IllegalArgumentException("Root must be not null.");
+        }
+        if (key == null) {
+            throw new IllegalArgumentException("Key must be not null.");
+        }
+        SimpleStack<Node<T>> stack = new SimpleStack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node<T> subNode = stack.pop();
+            if (subNode.getValue().equals(key)) {
+                return Optional.of(subNode);
+            }
+            for (Node<T> node : subNode.getChildren()) {
+                stack.push(node);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Node<T>> divideByKey(Node<T> root, T key) {
+        if (root == null) {
+            throw new IllegalArgumentException("Root must be not null.");
+        }
+        if (key == null) {
+            throw new IllegalArgumentException("Key must be not null.");
+        }
+        if (root.getValue().equals(key)) {
+            return Optional.of(root);
+        }
+        SimpleStack<Node<T>> stack = new SimpleStack<>();
+        stack.push(root);
+        Optional<Node<T>> result = Optional.empty();
+        while (!stack.isEmpty()) {
+            Node<T> subNode = stack.pop();
+            Iterator<Node<T>> iterator = subNode.getChildren().iterator();
+            while (iterator.hasNext()) {
+                Node<T> child = iterator.next();
+                if (child.getValue().equals(key)) {
+                    iterator.remove();
+                    return Optional.of(child);
+                }
+                stack.push(child);
             }
         }
         return result;
